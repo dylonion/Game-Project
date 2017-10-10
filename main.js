78f54cf47dff79f2d545bbb2c71f63d82f1b737d
Dylan Moylan <dylanmoylan1@gmail.com>
2017-10-10 13:18:29 -0400

    Fixes acceleration interval by merging its function into the main interval. Still needs work. Improves grow function and adds loss condition for getting eaten.

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ let board = {
   player: {
     x:350,
     y:200,
-    width:60,
+    w:60,
     height:60,
     selector:document.querySelector('#player'),
     keysDown: {
@@ -20,29 +20,37 @@ let board = {
     vx: 1,
     vy: 1,
     restSpeed:1,
-    grow: function(much){
-      this.width += much /2;
-      this.height += much /2;
-      this.selector.style.width = this.width + 'px';
+    grow: function(w,h,much){
+      this.w += w / 4;
+      this.height += h /4;
+      this.selector.style.width = this.w + 'px';
       this.selector.style.height = this.height + 'px';
-      board.score += much;
+      board.score += much /2;
       board.showScore();
       return;
     }
   },
   togglePause: function() {
     let paused = document.querySelector('.pause');
-    if(paused.className === "pause"){
+    if(paused.className === "pause overlay"){
       moveit = setInterval(move,this.refreshRate);
-      paused.className = "pause nodisplay";
+      spawnOne = setInterval(checkSpawns,6000);
+      paused.className = "pause overlay nodisplay";
     }else{
       clearInterval(moveit);
-      paused.setAttribute('class','pause');
+      clearInterval(spawnOne);
+      paused.setAttribute('class','pause overlay');
     }
   },
+  playerDie: function() {
+    clearInterval(moveit);
+    clearInterval(spawnOne);
+    document.getElementById('finalscore').innerHTML = this.score;
+    document.querySelector('.gameover').className = "gameover overlay";
+  },
   reset: function() {
-    let player = this.player;
-    player.width = 60;
+    let player = board.player;
+    player.w = 60;
     player.height = 60;
     player.x = 350;
     player.x = 200;
@@ -50,12 +58,16 @@ let board = {
     for(let i=0;i<this.npcs.length;i++){
       this.npcs[i].selector.remove();
     }
+    player.selector.style.height = player.height + 'px';
+    player.selector.style.width = player.w + 'px';
+    this.showScore();
     this.npcs = [];
     this.initNPCS();
     this.refreshRate = 25;
     changePosition(board.player);
     moveit = setInterval(move,this.refreshRate);
-    document.querySelector('.pause').className = "pause nodisplay";
+    document.querySelector('.pause').className += " nodisplay";
+    document.querySelector('.gameover').className += " nodisplay"
   },
   spawnNPC: function() {
     this.npcs.push(new Npc(-50,20,20,20));
@@ -65,7 +77,8 @@ let board = {
     let colors = ['red','blue','purple','#000']
     let count = 0;
     for(let i=0;i<200;i+=50){
-      this.npcs.push(new Npc(0,i,50,50));
+      let ran = randNum(10,50);
+      this.npcs.push(new Npc(0,i,ran,ran));
       this.npcs[this.npcs.length-1].htmlCreate('background-color:'+colors[count]);
       count++;
     }
@@ -73,6 +86,9 @@ let board = {
   showScore: function() {
     document.getElementById('score').innerHTML = board.score;
   }
+}
+const randNum = function(min,max){
+  return Math.floor(Math.random() * max) + min;
 }
 const Npc = function(x,y,width,height,selector){
   this.x = x;
@@ -92,6 +108,9 @@ const Npc = function(x,y,width,height,selector){
     this.selector.remove();
     this.x = -100;
     this.y = -100;
+  },
+  this.area = function() {
+    return this.width * this.height;
   }
 }
 const move = function() {
@@ -105,6 +124,18 @@ const move = function() {
 const movePlayer = function(boundaryX,boundaryY) {
   for(item in board.player.keysDown){
     if(board.player.keysDown[item]===true){
+      if(item === '65' || item === '68'){
+        if(board.player.vx <= 5){
+          board.player.vx +=.1;
+          console.log('increasing velocity',board.player.vx)
+        }
+      }
+      if(item === '87' || item === '83'){
+        if(board.player.vy <= 5){
+          board.player.vy +=.1;
+          console.log('increasing velocity',board.player.vx)
+        }
+      }
       switch(item){
         case '87'://W
           if(board.player.y >= 0){
@@ -112,14 +143,14 @@ const movePlayer = function(boundaryX,boundaryY) {
           }
           break;
         case '65'://A
-          if(board.player.x  > 0 - board.player.width / 2){
+          if(board.player.x  > 0 - board.player.w / 2){
             board.player.x -= board.player.vx;
           }else{
-            board.player.x = boundaryX - board.player.width;
+            board.player.x = boundaryX - board.player.w;
           }
           break;
         case '68'://D
-          if(board.player.x <= boundaryX - board.player.width / 2){
+          if(board.player.x <= boundaryX - board.player.w / 2){
             board.player.x += board.player.vx;
           }else{
             board.player.x = 0;
@@ -144,18 +175,17 @@ const changePosition = function(el) {
   if(el === board.player){
     let player = board.player;
     for(let i=0;i<board.npcs.length;i++){
-        if(board.npcs[i].x < player.x + player.width &&
+        if(board.npcs[i].x < player.x + player.w &&
            board.npcs[i].x + board.npcs[i].width > player.x &&
            board.npcs[i].y < player.y + player.height &&
            board.npcs[i].height + board.npcs[i].y > player.y)
         {
-          board.npcs[i].selector.className += ' collided';
-          if(player.width * player.height > board.npcs[i].width * board.npcs[i].height){
+          if(player.w * player.height > board.npcs[i].area()){
+            player.grow(board.npcs[i].width,board.npcs[i].height,board.npcs[i].area());
             board.npcs[i].kill();
-            player.grow(10);
+          }else{
+            board.playerDie();
           }
-        }else{
-          board.npcs[i].selector.className = board.npcs[i].selector.className.replace(/[ ]collided/,'')
         }
     }
   }
@@ -194,23 +224,29 @@ const checkY = function(el,boundary){
   itemToChange.y +=itemToChange.movementY;
   changePosition(itemToChange);
 }
-let accelerateInterval;
 const accelerateFunction = function(){
-  console.log('firing accelerateFunction');
-  accelerateInterval = setInterval(function(){
-    console.log('firing accelerateInterval');
-    if(board.player.vx > 0){
-      if(board.player.vx <= 5){
-        board.player.vx +=.2;
-      }
-    }else{
-      if(board.player.vx > -5){
-        board.player.vx -=.2;
-      }
-    }
-  },500);
+  if(Math.abs(board.player.vx) < 5){
+    console.log('firing accelerateFunction',Math.abs(board.player.vx));
+    accelerateInterval = setInterval(accelerate,500);
+  }else{
+    clearInterval(accelerateInterval);
+  }
 }
+
 let moveit;
+let spawnOne;
+const accelerate = function(){
+  console.log('firing accelerateInterval');
+  if(board.player.vx > 0){
+    if(board.player.vx <= 5){
+      board.player.vx +=.2;
+    }
+  }else{
+    if(board.player.vx > -5){
+      board.player.vx -=.2;
+    }
+  }
+}
 document.getElementById('accelerate').addEventListener('click',function(){
   clearInterval(moveit);
   board.refreshRate += 10;
@@ -222,9 +258,9 @@ document.getElementById('slow').addEventListener('click',function(){
   moveit = setInterval(move,board.refreshRate);
 })
 document.querySelector('body').addEventListener('keydown', function(e) {
-  if(e.which=== 65|| e.which === 68){
-    accelerateFunction();
-  }
+  // if(e.which=== 65|| e.which === 68){
+  //   accelerateFunction();
+  // }
   if(e.which === 27){
     board.togglePause();
   }
@@ -235,18 +271,10 @@ document.querySelector('body').addEventListener('keydown', function(e) {
   }
 });
 document.querySelector('body').addEventListener('keyup', function(e) {
-  if(e.which === 87 ||e.which === 65||e.which === 68 ||e.which === 83){
-    //if(typeof(accelerateInterval)!=="undefined"){
-      console.log('clearing accelerateInterval');
-      clearInterval(accelerateInterval);
-   // }
-    if(board.player.vx > 0){
-      board.player.vx = board.player.restSpeed;
-      console.log('setting vx to default;',board.player.vx);
-    }else{
-      board.player.vx = board.player.restSpeed * -1;
-      console.log('setting vx to default;',board.player.vx);
-    }
+  if(e.which === 65||e.which === 68){
+    board.player.vx = board.player.restSpeed;
+  }
+  if(e.which === 87||e.which === 83){
     board.player.vy = board.player.restSpeed;
   }
   for(item in board.player.keysDown){
@@ -258,18 +286,18 @@ document.querySelector('body').addEventListener('keyup', function(e) {
 document.querySelector('#resume').addEventListener('click',function(){
   board.togglePause();
 })
-document.querySelector('#newGame').addEventListener('click',function(){
-  board.reset();
+document.querySelectorAll('.newGame').forEach(function(e){
+  e.addEventListener('click',function(){
+    board.reset();
+  });
 })
-
-
 board.initNPCS();
-spawnOne = setInterval(function(){
+const checkSpawns = function() {
   if(board.npcs.length < 15){
     board.spawnNPC();
      console.log('spawnOne firing',board.npcs);
   }
-},8000)
+}
 // let randomize = setInterval(function() {
 //   var random_boolean = Math.random() >= 0.5;
 //   gol = random_boolean;
